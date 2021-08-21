ad41fe49b91563dbdd6ecd09c2b17de72a2cd933
Vedant B <vedantb@mkcl.org>
2021-08-21 13:46:06 +0530

    saving templates

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -17,6 +17,9 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Use(SessionLoad)
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
+	// mux.Handle("/static/*", http.StripPrefix("../../static/", mux))
 
 	return mux
 }
