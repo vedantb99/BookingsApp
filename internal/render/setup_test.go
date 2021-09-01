8f2ce8e21eabcb31a1e1fc732c75d2b591c38a2e
Vedant Balasubramaniam <59109429+vedantb99@users.noreply.github.com>
2021-09-01 12:00:21 +0530

    Lecture 95

--- internal/render/setup_test.go
+++ internal/render/setup_test.go
@@ -4,6 +4,7 @@ import (
 	"BookingsApp/internal/config"
 	"BookingsApp/internal/models"
 	"encoding/gob"
+	"log"
 	"net/http"
 	"os"
 	"testing"
@@ -19,6 +20,10 @@ func TestMain(m *testing.M) {
 	gob.Register(models.Reservation{})
 	//change this to true when in production
 	testApp.InProduction = false
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	testApp.InfoLog = infoLog
+	errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	testApp.ErrorLog = errorLog
 	session = scs.New()
 	session.Lifetime = 24 * time.Hour
 	session.Cookie.Persist = true
